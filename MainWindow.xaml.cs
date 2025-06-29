7db19c3cc8be7738a37d79fac7d8d86fa0f30394
Chill127 <shugeino12127@gmail.com>
2025-06-29 14:07:00 +0900

    Chatgptよりコードを整理。C#7.3に対応。 また、時間帯によってランダムにテキストを表示。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using EnaApp.Serif;
 
 namespace EnaApp
 {
     public partial class MainWindow : Window
     {
+        private SerifManager serifManager = new SerifManager();
+        private DispatcherTimer serifTimer = new DispatcherTimer();
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -13,6 +20,29 @@ namespace EnaApp
             // エナの画像ファイルを読み込む(ena.pngという名前でソリューションエクスプローラーに保存してある)
             var image = new BitmapImage(new System.Uri("ena.png", System.UriKind.Relative));
             EnaImage.Source = image;
+        }
+
+
+        private void EnaImage_RightClick(object sender, MouseButtonEventArgs e)
+        {
+            string line = serifManager.GetRandomSerif();
+            ShowSerif(line);
+        }
+
+        private void ShowSerif(string text)
+        {
+            SerifText.Text = text;
+            SerifText.Visibility = Visibility.Visible;
+
+            serifTimer.Stop();
+            serifTimer = new DispatcherTimer();
+            serifTimer.Interval = TimeSpan.FromSeconds(3);
+            serifTimer.Tick += delegate (object s, EventArgs e)
+            {
+                SerifText.Visibility = Visibility.Collapsed;
+                serifTimer.Stop();
+            };
+            serifTimer.Start();
         }
 
         // ドラッグで動くようにする。
@@ -23,5 +53,6 @@ namespace EnaApp
                 DragMove();
             }
         }
+
     }
 }
---
+++ serif/SerifManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnaApp.Serif
+{
+    public class SerifManager
+    {
+        private Dictionary<string, string[]> serifMap;
+
+        public SerifManager()
+        {
+            serifMap = new Dictionary<string, string[]>
+            {
+                { "morning", new[] { "おはようございます。今日はどうなさいますか？" } },
+                { "afternoon", new[] { "お昼です。きちんと昼食はとってくださいね" } },
+                { "evening", new[] { "お疲れ様です。もう夜になります。" } },
+                { "night", new[] { "もう遅いです。就寝の支度をされては？" } }
+            };
+        }
+
+        public string GetRandomSerif()
+        {
+            string key = GetTimePeriodKey();
+            string[] list;
+
+            if (!serifMap.TryGetValue(key, out list))
+            {
+                list = new[] { "…" };
+            }
+
+            Random rand = new Random();
+            return list[rand.Next(list.Length)];
+        }
+
+        private string GetTimePeriodKey()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 6 && hour < 12)
+                return "morning";
+            else if (hour >= 12 && hour < 18)
+                return "afternoon";
+            else if (hour >= 18 && hour < 24)
+                return "evening";
+            else
+                return "night";
+        }
+    }
+}

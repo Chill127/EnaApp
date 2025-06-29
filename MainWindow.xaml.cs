414e487b5a541e56b128f311fa31bb6dc74ffab3
A <A@DEZERTWOLF>
2025-06-29 13:08:44 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace EnaApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // エナの画像ファイルを読み込む（PNG形式、背景透過）
+            var image = new BitmapImage(new System.Uri("ena.png", System.UriKind.Relative));
+            EnaImage.Source = image;
+        }
+
+        // ウィンドウドラッグの処理
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
+}
